ce7f8e5676f2c23fad20b9b9a71e12801db23d52
jeremycartesse <cartessejeremy@gmail.com>
2017-04-23 17:59:38 +0200

    add famille

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420053800) do
+ActiveRecord::Schema.define(version: 20170423154836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 20170420053800) do
     t.string   "photo_avatar"
     t.string   "nationality"
     t.text     "description"
+    t.string   "famille"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
